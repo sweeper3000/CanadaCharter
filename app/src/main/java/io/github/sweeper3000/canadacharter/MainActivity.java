d3d776006c9d9c043a3fd6822d9bdd260b791b11
sweeper3000 <spiketwofish@protonmail.com>
2016-12-06 16:31:36 -0500

    Initial commit

---
+++ app/src/main/java/io/github/sweeper3000/canadacharter/MainActivity.java
@@ -0,0 +1,15 @@
+package io.github.sweeper3000.canadacharter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
