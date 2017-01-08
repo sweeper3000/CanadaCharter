73ca7be9a0c31561dc1edf00f89de6cd5cc8ab83
sweeper3000 <spiketwofish@protonmail.com>
2017-01-07 23:22:09 -0500

    Add way to view the code in the app

--- app/src/main/java/io/github/sweeper3000/canadacharter/About.java
+++ app/src/main/java/io/github/sweeper3000/canadacharter/About.java
@@ -1,7 +1,11 @@
 package io.github.sweeper3000.canadacharter;
 
+import android.net.Uri;
+import android.support.customtabs.CustomTabsIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.graphics.Color;
 
 public class About extends AppCompatActivity {
 
@@ -10,4 +14,16 @@ public class About extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
     }
+
+    // Go to GitHub
+
+        public void toGitHub(View view) {
+            final String GITHUB = "https://github.com/sweeper3000/CanadaCharter";
+            final int CUSTOM_TABS_TOOLBAR_COLOR = Color.parseColor("#F44336");
+
+            CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+            CustomTabsIntent github_intent = builder.build();
+            builder.setToolbarColor(CUSTOM_TABS_TOOLBAR_COLOR);
+            github_intent.launchUrl(this, Uri.parse(GITHUB));
+        }
 }
